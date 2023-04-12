09e5a703930489096cf02cc25a8417de28f2cda0
RuggeroCoco <ruggerococo94@gmail.com>
2023-04-12 20:06:13 +0200

    Milestone 1

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      emails: [],
+    };
+  },
+  mounted() {
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((res) => this.emails.push(res.data.response));
+    }
+  },
+}).mount("#app");
